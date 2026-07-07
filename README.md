30f2c3eccca51656238658bd9a6ee8ba6d2b9438
Sharon Mae Duran <sharonmaeduran@MBP-di-Sharon.home-life.hub>
2026-07-07 19:57:52 +0200

    add files

---
+++ README.md
@@ -0,0 +1,134 @@
+# Azure IaC Infrastructure
+
+## Descrizione
+
+**Azure IaC Infrastructure** è un progetto dedicato all'implementazione di infrastrutture cloud su **Microsoft Azure** mediante il paradigma **Infrastructure as Code (IaC)**.
+
+L'obiettivo del repository è automatizzare il provisioning delle risorse Azure utilizzando **Terraform**, garantendo configurazioni ripetibili, versionabili e facilmente manutenibili.
+
+Il progetto rappresenta una base per la gestione dell'infrastruttura cloud secondo le best practice DevOps, favorendo automazione, scalabilità e controllo delle modifiche attraverso Git.
+
+---
+
+## Obiettivi del progetto
+
+* Automatizzare il provisioning delle risorse Azure.
+* Gestire l'infrastruttura come codice (Infrastructure as Code).
+* Rendere le configurazioni versionabili tramite Git.
+* Facilitare il rilascio di ambienti riproducibili.
+* Adottare un approccio modulare e scalabile per futuri sviluppi.
+
+---
+
+## Tecnologie utilizzate
+
+* **Terraform**
+* **Microsoft Azure**
+* **Azure CLI**
+* **YAML**
+* **Git & GitHub**
+
+---
+
+## Struttura del repository
+
+```text
+azure-iac-infrastructure/
+│
+├── azure_vm.tf          # Configurazione Terraform della Virtual Machine
+├── azure_vm.yml         # File di configurazione YAML
+├── README.md            # Documentazione del progetto
+└── .gitignore
+```
+
+---
+
+## Prerequisiti
+
+Per utilizzare il progetto sono necessari:
+
+* Terraform installato
+* Azure CLI
+* Un account Microsoft Azure con una sottoscrizione attiva
+* Git
+
+---
+
+## Utilizzo
+
+### 1. Clonare il repository
+
+```bash
+git clone https://github.com/<username>/azure-iac-infrastructure.git
+cd azure-iac-infrastructure
+```
+
+### 2. Autenticarsi su Azure
+
+```bash
+az login
+```
+
+### 3. Inizializzare Terraform
+
+```bash
+terraform init
+```
+
+### 4. Verificare il piano di esecuzione
+
+```bash
+terraform plan
+```
+
+### 5. Distribuire l'infrastruttura
+
+```bash
+terraform apply
+```
+
+---
+
+## Caratteristiche
+
+* Provisioning automatico delle risorse Azure.
+* Configurazione dell'infrastruttura tramite Terraform.
+* Infrastruttura completamente versionata con Git.
+* Codice facilmente estendibile e manutenibile.
+* Struttura predisposta per l'integrazione con pipeline CI/CD.
+
+---
+
+## Sviluppi futuri
+
+Il repository potrà essere ampliato con:
+
+* Virtual Network (VNet)
+* Subnet
+* Network Security Groups (NSG)
+* Public IP
+* Storage Account
+* Azure Key Vault
+* Managed Identity
+* Terraform Modules
+* Pipeline GitHub Actions o Azure DevOps
+* Gestione di ambienti multipli (Development, Test, Production)
+
+---
+
+## Best Practice adottate
+
+* Infrastructure as Code (IaC)
+* Versionamento tramite Git
+* Configurazioni dichiarative
+* Automazione del provisioning
+* Repository organizzato e documentato
+* Approccio orientato alla scalabilità
+
+---
+
+## Autore
+
+Progetto realizzato a scopo di studio, sperimentazione e consolidamento delle competenze in ambito **Cloud Computing**, **Microsoft Azure**, **Terraform** e **DevOps**.
+
+---
---
+++ azure_vm.tf
@@ -0,0 +1,136 @@
+# Terraform
+
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "rg-demo"
+}
+
+variable "vm_name" {
+  default = "demo-vm"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "demo-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "demo-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "demo-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+
+  admin_username = var.admin_username
+  admin_password = var.admin_password
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.vm.id
+}
